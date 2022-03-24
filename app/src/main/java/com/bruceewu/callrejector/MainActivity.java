f2e90168d7593bfa4ea2fade49f7d299f7351037
jun <jun.wu@xinfei.cn>
2022-03-24 14:43:37 +0800

    1.获取拨入的手机号

--- app/src/main/java/com/bruceewu/callrejector/MainActivity.java
+++ app/src/main/java/com/bruceewu/callrejector/MainActivity.java
@@ -1,10 +1,16 @@
 package com.bruceewu.callrejector;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.bruceewu.callrejector.utils.CallService;
+import com.bruceewu.callrejector.utils.LogUtils;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startService(new Intent(this, CallService.class));
+        checkPermission();
     }
 
+    @SuppressLint("CheckResult")
     private void checkPermission() {
+        new RxPermissions(this)
+                .request(Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_CALL_LOG)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(grant -> {
+                    if (grant) {
+                        register();
+                    } else {
+                        LogUtils.log("Permission denied");
+                    }
+                }, error -> {
+                    LogUtils.log(error.getMessage());
+                });
+    }
 
+    private void register() {
+        startService(new Intent(this, CallService.class));
     }
 }
